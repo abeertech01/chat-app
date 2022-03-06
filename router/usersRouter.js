da914f1139126e26446aa5f27ba02feae39e469c
Abeer <abeer.technology@gmail.com>
2022-03-06 13:21:01 +0600

    profile picture upload and adding user - backend

--- router/usersRouter.js
+++ router/usersRouter.js
@@ -1,13 +1,28 @@
 // external imports
 const express = require("express");
+const { check } = require("express-validator");
 
 //inter exports
-const { getUsers } = require("../controller/usersController.js");
+const { getUsers, addUser } = require("../controller/usersController.js");
 const decorateHtmlResponse = require("../middlewares/common/decorateHtmlResponse.js");
+const avatarUpload = require("../middlewares/users/avatarUpload.js");
+const {
+  addUserValidators,
+  addUserValidationHandler,
+} = require("../middlewares/users/userValidators.js");
 
 const router = express.Router();
 
 //users page
 router.get("/", decorateHtmlResponse("Users"), getUsers);
+
+// add user
+router.post(
+  "/",
+  avatarUpload,
+  addUserValidators,
+  addUserValidationHandler,
+  addUser
+);
 
 module.exports = router;
